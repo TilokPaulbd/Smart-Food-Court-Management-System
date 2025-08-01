4af4ff7814e3e18b05050492a9c34a4ba38c6243
TilokPaulbd <tilokpaulbd@gmail.com>
2025-08-01 23:14:53 +0600

    student class added

---
+++ Student.java
@@ -0,0 +1,35 @@
+public class Student extends User {
+    private Order currentOrder;
+    
+    public Student(String id,String name){
+        super(id, name);
+    }
+    
+    public void displayMenu(){
+        FoodCourtSystem.displayMenu();
+    }
+
+    public void startNewOrder(){
+        this.currentOrder=new Order(this.id);
+    }
+    public Order getCurrentOrder(){
+        return currentOrder;
+    }
+
+    public void addToOrder(FoodItem foodItem,int quantity){
+        if(currentOrder==null){
+            startNewOrder();
+        }
+        else{
+            currentOrder.addItem(new OrderItem(foodItem, quantity));
+        }
+    }
+
+
+    public void checkOut(){
+        if (currentOrder !=null) {
+            currentOrder.markAsPaid();
+            currentOrder=null;
+        }
+    }
+}
